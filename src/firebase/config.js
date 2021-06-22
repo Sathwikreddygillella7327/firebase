a595587a5203161877382124e990802970cf22c4
Sathwikreddygillella7327 <gillellar.ug19.cs@nitp.ac.in>
2021-06-22 16:38:12 +0530

    firebase

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,8 +1,9 @@
 export const config = {
-  apiKey: "Your API Key",
-  authDomain: "Your Auth Domain",
-  databaseURL: "Your Database URL",
-  projectId: "Your Project ID",
-  storageBucket: "Your Storage Bucket",
-  messagingSenderId: "Your Messaging Sender ID"
+  apiKey: "AIzaSyBEeDzyIYCXEFrKq05y8bgpiFYDwkitx-Y",
+    authDomain: "confusionserver-27f73.firebaseapp.com",
+    projectId: "confusionserver-27f73",
+    storageBucket: "confusionserver-27f73.appspot.com",
+    messagingSenderId: "728036473774",
+    appId: "1:728036473774:web:78bd0d9c75a05341f2663f",
+    measurementId: "G-5Q7PQ6TLEQ"
 };
